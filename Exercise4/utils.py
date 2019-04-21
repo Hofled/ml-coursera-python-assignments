Progressed with exercise4, got to backprop implementation part using a vectorized method. The implementation is not correct yet, but runs without errors (with correct dimensions of matrices and vectors). The part that still needs fixing is the calculation of the gradients of the cost function for each layer.

Exercise4/utils.py
@@ -154,9 +154,9 @@ def checkNNGradients(nnCostFunction, lambda_=0):
     # short hand for cost function
     costFunc = lambda p: nnCostFunction(p, input_layer_size, hidden_layer_size,
                                         num_labels, X, y, lambda_)
+    
     cost, grad = costFunc(nn_params)
     numgrad = computeNumericalGradient(costFunc, nn_params)
-
     # Visually examine the two gradient computations.The two columns you get should be very similar.
     print(np.stack([numgrad, grad], axis=1))
     print('The above two columns you get should be very similar.')